46fa949d91d190541830bbb663ae39b1060f9501
wrzes <w.rzeszutek@gmail.com>
2023-01-28 14:33:24 +0100

    players_unlikely_to_play implemented

--- lineup_fetcher.py
+++ lineup_fetcher.py
@@ -3,18 +3,38 @@ from bs4 import BeautifulSoup
 
 
 def players_unlikely_to_play():
-    global url, name
     url = "https://www.rotowire.com/basketball/nba-lineups.php"
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    li_elements = soup.find_all("li", class_="is-pct-play-50")
+
+    li_75pct_elements = soup.find_all("li", class_="is-pct-play-75")
+    li_50pct_elements = soup.find_all("li", class_="is-pct-play-50")
+    li_25pct_elements = soup.find_all("li", class_="is-pct-play-25")
+    li_0pct_elements = soup.find_all("li", class_="is-pct-play-0")
+
     players = []
-    for li_element in li_elements:
-        players.append(li_element.find('a'))
-    for player in players:
-        name = player.get_text()
-        print(name)
+
+    for element in li_75pct_elements:
+        players.append({"name": element.find('a').get_text(),
+                        "team": element.find_parent("div", class_="lineup__box").find("div", class_="lineup__abbr").get_text(),
+                        "likelihood": 75})
+
+    for element in li_50pct_elements:
+        players.append({"name": element.find('a').get_text(),
+                        "team": element.find_parent("div", class_="lineup__box").find("div", class_="lineup__abbr").get_text(),
+                        "likelihood": 50})
+
+    for element in li_25pct_elements:
+        players.append({"name": element.find('a').get_text(),
+                        "team": element.find_parent("div", class_="lineup__box").find("div", class_="lineup__abbr").get_text(),
+                        "likelihood": 25})
+
+    for element in li_0pct_elements:
+        players.append({"name": element.find('a').get_text(),
+                        "team": element.find_parent("div", class_="lineup__box").find("div", class_="lineup__abbr").get_text(),
+                        "likelihood": 0})
+    return players
 
 
 if __name__ == "__main__":
-    players_unlikely_to_play()
+    print(players_unlikely_to_play())
